2a916ea5537e7814552d42832fe99e8e4ba824f8
NocturneLight <55106060+NocturneLight@users.noreply.github.com>
2025-03-30 17:10:15 -0500

    Revert "- Added test login system."
    This reverts commit de9be9595008dd7a6c167529ab4a2232f1fc86b3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
-import { AmplifyAuthenticatorModule, AuthenticatorService } from "@aws-amplify/ui-angular"
 
 Amplify.configure(outputs);
 
@@ -12,12 +11,8 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
+  imports: [RouterOutlet, TodosComponent],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
-
-  constructor(public authenticator: AuthenticatorService) {
-    Amplify.configure(outputs);
-  }
 }
